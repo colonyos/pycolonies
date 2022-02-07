9d423b7370ed6515231e80e780fedbc12bc355c9
Johan Kristiansson <johan.kristiansson@protonmail.com>
2022-02-07 14:59:28 +0100

    more impl

--- colonies.py
+++ colonies.py
@@ -28,22 +28,104 @@ class Colonies:
         base64_payload = reply_msg_json["payload"]
         payload_bytes = base64.b64decode(base64_payload)
         payload = json.loads(payload_bytes)
-        
+       
         if reply.status_code == 200:
             return payload
         else:
             raise Exception(payload["message"])
-    
+
     def add_colony(self, colony, prvkey):
-        add_colony_msg = {
+        msg = {
             "msgtype": "addcolonymsg",
             "colony": colony
         }
-        return self.__rpc(add_colony_msg, prvkey)
+        return self.__rpc(msg, prvkey)
     
     def del_colony(self, colonyid, prvkey):
-        add_colony_msg = {
+        msg = {
             "msgtype": "deletecolonymsg",
             "colonyid": colonyid
         }
-        return self.__rpc(add_colony_msg, prvkey)
+        return self.__rpc(msg, prvkey)
+    
+    def list_colonies(self, prvkey):
+        msg = {
+            "msgtype": "getcoloniesmsg",
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def get_colony(self, colonyid, prvkey):
+        msg = {
+            "msgtype": "getcolonymsg",
+            "colonyid": colonyid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def get_colony(self, colonyid, prvkey):
+        msg = {
+            "msgtype": "getcolonymsg",
+            "colonyid": colonyid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def add_runtime(self, runtime, prvkey):
+        msg = {
+            "msgtype": "addruntimemsg",
+            "runtime": runtime
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def list_runtimes(self, colonyid, prvkey):
+        msg = {
+            "msgtype": "getruntimesmsg",
+            "colonyid": colonyid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def approve_runtime(self, runtimeid, prvkey):
+        msg = {
+            "msgtype": "approveruntimemsg",
+            "runtimeid": runtimeid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def reject_runtime(self, runtimeid, prvkey):
+        msg = {
+            "msgtype": "rejectruntimemsg",
+            "runtimeid": runtimeid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def delete_runtime(self, runtimeid, prvkey):
+        msg = {
+            "msgtype": "deleteruntimemsg",
+            "runtimeid": runtimeid
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def submit_process_spec(self, process_spec, prvkey):
+        msg = {
+            "msgtype": "submitprocessespecmsg",
+            "spec": process_spec
+        }
+        return self.__rpc(msg, prvkey)
+    
+    def assign_process(self, colonyid, prvkey):
+        msg = {
+            "msgtype": "assignprocessmsg",
+            "colonyid": colonyid
+        }
+        return self.__rpc(msg, prvkey)
+  
+    # state 0 : Waiting
+    # state 1 : Running
+    # state 2 : Success
+    # state 3 : Failed
+    def list_processes(self, colonyid, count, state, prvkey):
+        msg = {
+            "msgtype": "getprocessesmsg",
+            "colonyid": colonyid,
+            "count": count,
+            "state": state
+        }
+        return self.__rpc(msg, prvkey)
--- colonies_test.py
+++ colonies_test.py
@@ -5,37 +5,172 @@ from crypto import Crypto
 from colonies import Colonies
 
 class TestColonies(unittest.TestCase):
-
-    def test_add_colony(self):
+    def setUp(self):
         url = "https://10.0.0.240:8080/api"
-        client = Colonies(url)
+        self.client = Colonies(url)
+        self.crypto = Crypto()
+        self.server_prv = "09545df1812e252a2a853cca29d7eace4a3fe2baad334e3b7141a98d43c31e7b"
 
-        crypto = Crypto()
-        prvkey = crypto.prvkey()
-        colonyid = crypto.id(prvkey)
-
+    def add_test_colony(self):
+        colony_prvkey = self.crypto.prvkey()
+        colonyid = self.crypto.id(colony_prvkey)
         colony = {
             "colonyid" : colonyid,
             "name" : "python_test"
         }
 
-        server_prv = "09545df1812e252a2a853cca29d7eace4a3fe2baad334e3b7141a98d43c31e7b"
-        added_colony = client.add_colony(colony, server_prv)
+        return self.client.add_colony(colony, self.server_prv), colonyid, colony_prvkey
+    
+    def add_test_runtime(self, colonyid, colony_prvkey):
+        runtime_prvkey = self.crypto.prvkey()
+        runtimeid = self.crypto.id(runtime_prvkey)
+
+        runtime = {
+            "name": "test_runtime",
+            "runtimeid": runtimeid,
+            "colonyid": colonyid,
+            "runtimetype": "test_runtime_type",
+            "cpu": "AMD Ryzen 9 5950X (32) @ 3.400GHz",
+            "cores": 32,
+            "mem": 80326,
+            "gpu": "NVIDIA GeForce RTX 2080 Ti Rev. A",
+            "gpus": 1
+        }
+
+        return self.client.add_runtime(runtime, colony_prvkey), runtimeid, runtime_prvkey
+    
+    def submit_test_process(self, colonyid, runtime_prvkey):
+        process_spec = {
+            "conditions": {
+                "colonyid": colonyid,
+                "runtimeids": [],
+                "runtimetype": "test_runtime_type",
+                "mem": 1000,
+                "cores": 1,
+                "gpus":0
+            },
+            "env": {
+                "test_key": "test_value2"
+            },
+            "timeout": -1,
+            "maxretries": 3
+        }
+    
+        return self.client.submit_process_spec(process_spec, runtime_prvkey)
+
+    def test_add_colony(self):
+        added_colony, colonyid, _ = self.add_test_colony()
         self.assertEqual(added_colony["colonyid"], colonyid)
 
-        client.del_colony(colonyid, server_prv)
+        self.client.del_colony(colonyid, self.server_prv)
 
         server_prv = "09545df1812e252a2a853cca29d7eace4a3fe2baad334e3b7141a98d43c31e7c"  # note last c, incorrect key
         with self.assertRaises(Exception): 
             client.add_colony(colony, server_prv)
-    
+   
     def test_del_colony(self):
-        url = "https://10.0.0.240:8080/api"
-        client = Colonies(url)
-        server_prv = "09545df1812e252a2a853cca29d7eace4a3fe2baad334e3b7141a98d43c31e7b"
-        colonyid = "09545df1812e252a2a853cca29d7eace4a3fe2baad334e3b7141a98d43c31e7b"
-        client.del_colony(colonyid, server_prv)
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        colonies_from_server = self.client.list_colonies(self.server_prv)
+      
+        found_colony = False
+        for colony in colonies_from_server:
+            if colony["colonyid"] == colonyid:
+                found_colony = True
+        self.assertTrue(found_colony)
 
+        self.client.del_colony(colonyid, self.server_prv)
+        colonies_from_server = self.client.list_colonies(self.server_prv)
+        
+        found_colony = False
+        for colony in colonies_from_server:
+            if colony["colonyid"] == colonyid:
+                found_colony = True
+        self.assertFalse(found_colony)
+    
+    def test_add_runtime(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+
+        self.assertEqual(runtimeid, added_runtime["runtimeid"])
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_list_runtimes(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server[0]["runtimeid"], runtimeid)
+
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_approve_runtime(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server[0]["state"], 0)
+    
+        self.client.approve_runtime(runtimeid, colony_prvkey)
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server[0]["state"], 1)
+
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_reject_runtime(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server[0]["state"], 0)
+    
+        self.client.reject_runtime(runtimeid, colony_prvkey)
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server[0]["state"], 2)
+
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_delete_runtime(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+
+        self.client.delete_runtime(runtimeid, colony_prvkey)
+        runtimes_from_server = self.client.list_runtimes(colonyid, colony_prvkey)
+        self.assertEqual(runtimes_from_server, None)  # XXX: Shouldn't list_runtimes return an empty list rather than None?
+
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_submit_process(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+        self.client.approve_runtime(runtimeid, colony_prvkey)
+        process = self.submit_test_process(colonyid, runtime_prvkey)
+        self.assertEqual(process["state"], 0)
+        
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_assign_process(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+        self.client.approve_runtime(runtimeid, colony_prvkey)
+        process = self.submit_test_process(colonyid, runtime_prvkey)
+       
+        assigned_process = self.client.assign_process(colonyid, runtime_prvkey)
+        self.assertEqual(assigned_process["processid"], process["processid"])
+
+        self.client.del_colony(colonyid, self.server_prv)
+    
+    def test_list_process(self):
+        added_colony, colonyid, colony_prvkey = self.add_test_colony()
+        added_runtime, runtimeid, runtime_prvkey = self.add_test_runtime(colonyid, colony_prvkey)
+        self.client.approve_runtime(runtimeid, colony_prvkey)
+ 
+        self.submit_test_process(colonyid, runtime_prvkey)
+        self.submit_test_process(colonyid, runtime_prvkey)
+
+        waiting_processes = self.client.list_processes(colonyid, 2, 0, runtime_prvkey)
+        self.assertEqual(len(waiting_processes), 2)
+
+        self.client.del_colony(colonyid, self.server_prv)
 
 if __name__ == '__main__':
     unittest.main()
