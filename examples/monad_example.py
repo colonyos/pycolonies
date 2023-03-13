8cb4201c5634c5aaa06b3a69723d79ca98f0a262
Johan Kristiansson <kristiansson.johan@gmail.se>
2023-03-13 11:58:24 +0100

    updated monad example

--- examples/monad_example.py
+++ examples/monad_example.py
@@ -8,14 +8,15 @@ colonyid = "4787a5071856a4acf702b2ffcea422e3237a679c681314113d86139461290cf4"
 executorid = "3fc05cf3df4b494e95d6a3d297a34f19938f7daa7422ab0d4f794454133341ac" 
 executor_prvkey = "ddf7f7791208083b6a9ed975a72684f6406a269cfa36f1b1c32045c0a71fff05"
 
-def gen_nums(ctx={}):
+def gen_data(ctx={}):
     return 1, 2 
 
-def reduce(*nums, ctx={}):
+def process_data(*nums, ctx={}):
     total = 0
     for n in nums:
         total += n
     return total 
 
 m = ColoniesMonad("localhost", 50080, colonyid, executor_prvkey)
-print((m >> gen_nums >> reduce).unwrap())  # returns 1 + 2 = 3
+result = (m >> gen_data >> process_data).unwrap()
+print(result)  # prints 3 
